d27d2cec027f16d26f6059c4b9d020c9bde56c5f
Chtistianlean <you@example.com>
2017-04-12 16:36:57 +0800

    second commit

---
+++ app/src/main/java/com/example/administrator/asynctasktest/NewsBean.java
@@ -0,0 +1,12 @@
+package com.example.administrator.asynctasktest;
+
+/**
+ * Created by Administrator on 2017/4/7.
+ */
+
+public class NewsBean {
+    public String newsIconUrl;
+    public String newsTitle;
+    public String newsContent;
+
+}
